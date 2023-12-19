ce4aef5e3640a72b9fe77a52a3ad8e9c7905aee0
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2023-12-19 12:01:17 +0300

    3d commit

--- YaGPT-RAG-bot.py
+++ YaGPT-RAG-bot.py
@@ -256,6 +256,13 @@ def main():
                     # # обновляем историю чата с помощью вопроса пользователя и ответа от бота
                     st.session_state['chat_history'].append(
                         {"вопрос": user_input, "ответ": output['result']})
+                    ## добавляем источники к ответу
+                    input_documents = output['source_documents']
+                    for doc in input_documents:
+                        source = doc.metadata['source']
+                        page_content = doc.page_content
+                        with st.expander(f"**Источник:** [{source}]"):
+                            st.write(page_content)
 
         if st.session_state['generated']:
             with response_container:
